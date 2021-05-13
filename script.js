6a9f0ae3c6fdf5adb764d7fcf03d89e256e069fe
Akshit Sharma <waprajbhasha9@gmail.com>
2021-05-13 10:15:21 +0530

    commit 4
    added sex

--- script.js
+++ script.js
@@ -6,6 +6,11 @@ $(document).ready(function(){
         else{
             $('.navbar').removeClass("sticky");
         }
-    })
-})
+    });
+    /*toggle menu/navbar script*/
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+    });
+
+});
 
